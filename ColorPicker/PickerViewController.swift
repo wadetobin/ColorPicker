be3f9b2052503df8e5739d610844383377b2e5df
Wade Tobin <wadetobin@mwc-101109.dhcp.missouri.edu>
2017-11-16 15:43:01 -0600

    Color PickerView Challenge finished

---
+++ ColorPicker/PickerViewController.swift
@@ -0,0 +1,67 @@
+//
+//  PickerViewController.swift
+//  ColorPicker
+//
+//  Created by Wade Tobin on 11/16/17.
+//  Copyright © 2017 Wade Tobin. All rights reserved.
+//
+
+import UIKit
+
+class PickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+    let bgColors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = bgColors[0]
+        textLabel.text = colors[0]
+    
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        textLabel.text = colors[row]
+        let bgColor = bgColors[row]
+        
+        view.backgroundColor = bgColor
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ColorPicker/colors.swift
@@ -0,0 +1,15 @@
+//
+//  colors.swift
+//  ColorPicker
+//
+//  Created by Wade Tobin on 11/16/17.
+//  Copyright © 2017 Wade Tobin. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
+
